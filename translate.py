d38d0bb0db2d392be224f6d0f3abe8a88dd40afb
Abdurrahman YAKAR <ryakar75@gmail.com>
2024-10-14 11:56:33 +0300

    first commit

--- translate.py
+++ translate.py
@@ -1,9 +1,6 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np
+import pandas as pd
 from googletrans import Translator
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
-
 
 # Excel dosyasını okuma
 file_path = './data/simplified_liar_train_dataset.xlsx'
@@ -12,18 +9,24 @@ df = pd.read_excel(file_path)
 # Translator nesnesi
 translator = Translator()
 
+
 # 2. satırdan sonuna kadar D sütunundaki (Statement) verileri çevirme işlemi
-def translate_cell(text):
+def translate_cell(text, index):
     try:
         if pd.isnull(text):
-            return ""  # Eğer hücre boşsa
+            print(f"{index}. satır boş, atlanıyor.")
+            return ""  # Eğer hücre boşsa boş döndür
         translated = translator.translate(text, src='en', dest='tr')
+        print(f"{index}. satır başarıyla çevrildi.")
         return translated.text
     except Exception as e:
+        print(f"{index}. satır çevirilirken hata oluştu: {str(e)}")
         return str(e)  # Hata durumunda hatayı geri döndür
 
+
 # 2. satırdan itibaren tüm satırların Statement sütunundaki verileri çeviriyoruz
-df.loc[0:, 'Türkçe Metin'] = df.loc[0:, 'Statement'].apply(translate_cell)
+for index, row in df.iterrows():
+    df.at[index, 'Türkçe Metin'] = translate_cell(row['Statement'], index)
 
 # Yeni tabloyu kaydedelim
 output_path = './output_file/translated_liar_train_dataset.xlsx'
